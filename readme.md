e6c5eb391c9df40b98365685e046dc5340d2781c
VRU2306 <60779508+VRU2306@users.noreply.github.com>
2020-05-11 11:08:59 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,3 @@
+# usePasswordValidationHook
+
+A simple example of custom react hooks to implement reusable password validation
---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const usePasswordValidation = initialValue => {
+    const [password, setPassword] = React.useState(initialValue);
+    const [error, setError] = React.useState(undefined);
+
+    const validatePassword = value => {
+        let passwordError = undefined;
+        if (!value.length) {
+            passwordError = "Please enter a value";
+        } else if (password && !/^.{5,15}$/.test(value)) {
+            passwordError = "Password should be between 5 and 15 characters";
+        }
+        if (!passwordError) {
+            setPassword(value);
+        }
+        setError(passwordError);
+    };
+
+    return [error, password, validatePassword];
+};
+
+function App() {
+    const [
+        currentPasswordError,
+        currentPassword,
+        validateCurrentPassword
+    ] = usePasswordValidation("");
+    const [
+        newPasswordError,
+        newPassword,
+        validateNewPassword
+    ] = usePasswordValidation("");
+    const [
+        confirmedPasswordError,
+        confirmedPassword,
+        validateConfirmedPassword
+    ] = usePasswordValidation("");
+
+    const allPasswordsSet =
+        currentPassword.length > 0 &&
+        newPassword.length > 0 &&
+        confirmedPassword.length > 0;
+
+    return (
+        <div className="App">
+            <h1>AthrV-Ed LMS Portal</h1>
+            <div className="formField">
+                <h3>New Password</h3>
+                <input
+                    type="password"
+                    onKeyUp={e => {
+                        validateNewPassword(e.target.value);
+                    }}
+                    defaultValue={newPassword}
+                    placeholder="New Password"
+                />
+                {!!newPasswordError && (
+                    <label className="error">{newPasswordError}</label>
+                )}
+            </div>
+            <div className="formField">
+                <h3>Confirm Password</h3>
+                <input
+                    type="password"
+                    onKeyUp={e => {
+                        validateConfirmedPassword(e.target.value);
+                    }}
+                    defaultValue={confirmedPassword}
+                    placeholder="Confirm Password"
+                />
+                {!!confirmedPasswordError && (
+                    <label className="error">{confirmedPasswordError}</label>
+                )}
+            </div>
+            <div className="formField">
+                <button
+                    type="button"
+                    onClick={() => {
+                        alert("Your password has been changed");
+                    }}
+                >
+                    Submit
+        </button>
+            </div>
+        </div>
+    );
+}
+export default App;
